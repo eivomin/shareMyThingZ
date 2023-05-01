99ddad6c977c4c2f491c0c48021d2819198a7c38
eivomin <eivomiin@gmail.com>
2023-05-01 09:32:19 +0900

    Initial commit

---
+++ src/main/java/com/hanghae/sharemythingz/ShareMyThingZApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.sharemythingz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMyThingZApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareMyThingZApplication.class, args);
+    }
+
+}
